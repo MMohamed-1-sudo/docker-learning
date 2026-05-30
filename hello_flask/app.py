719614ae4a88f3dc1cce33addd412b225072c2ac
Mohamed.M <MMohamed_786@outlook.com>
2026-05-30 19:27:17 +0100

    docker networking and linking

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -1,12 +1,23 @@
+# app.py
+
 from flask import Flask
-
-
+import MySQLdb
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, world!'
+    # Connect to the MySQL database
+    db = MySQLdb.connect(
+    host="mydb",
+    user="root",
+    passwd="root",
+    db="mydatabase"
+)
+    cur = db.cursor()
+    cur.execute("SELECT VERSION()")
+    version = cur.fetchone()
+    return f'Hello, World! MySQL version: {version[0]}'
 
-if  __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001) 
